f9450bad06a8c011c180b823ad47e814d0ef01b3
wwq345 <wwq.bst@sap.com>
2022-03-14 14:15:52 +0800

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by i564206 on 2022/3/14.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
